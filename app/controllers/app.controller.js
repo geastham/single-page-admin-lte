116cc4ae1e64fd44fb671f674f718b772fa8b8e4
Garrett Eastham <garrett.eastham@gmail.com>
2016-10-18 00:07:38 -0500

    setup initial app controller and rendered some dynamic content

---
+++ app/controllers/app.controller.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Main app controller
+angular.module('apj-browser').controller('AppController', ['$scope',
+  function ($scope) {
+    console.log("Loaded app controller...");
+
+    $scope.phones = [
+      {
+        name: 'Nexus S',
+        snippet: 'Fast just got faster with Nexus S.'
+      }, {
+        name: 'Motorola XOOM™ with Wi-Fi',
+        snippet: 'The Next, Next Generation tablet.'
+      }, {
+        name: 'MOTOROLA XOOM™',
+        snippet: 'The Next, Next Generation tablet.'
+      }
+    ];
+  }
+]);
